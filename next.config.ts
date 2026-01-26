e42f7ec7ac0f174ae5a4de3c4c725a5ac5386de9
romain_g <romain-girardi@hotmail.fr>
2026-01-26 01:17:44 +0100

    Add /privacy redirect to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: '/privacy',
+        destination: '/fr/privacy',
+        permanent: false,
+      },
+    ];
+  },
   async headers() {
     return [
       {
